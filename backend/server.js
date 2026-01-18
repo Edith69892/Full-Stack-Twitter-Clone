f29a786e21f521a628e2ab2eccad9be4c0c423e1
Roshan Patil <patilroshan9687@gmail.com>
2026-01-18 16:45:53 +0530

    connect mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,24 @@
 import express from 'express';
 import cors from 'cors';
 import {app} from "./app.js"
+import dotenv from 'dotenv/config';
+import mongoose from 'mongoose';  
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+dotenv.config({
+  path : "./.env"
 });
+
+;(async () =>{
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URI}/X-Clone`)
+
+    app.listen(process.env.PORT, () => {
+  console.log(`Server is running on http://localhost:${process.env.PORT}`);
+});
+  } catch (error) {
+    console.error("Error connecting to the database:", error);  
+  }
+})()
+
