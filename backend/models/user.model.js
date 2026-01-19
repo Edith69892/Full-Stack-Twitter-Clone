c7066c6594850b6874600429d28997dbe08bd329
Roshan Patil <patilroshan9687@gmail.com>
2026-01-19 16:51:32 +0530

    write user model

---
+++ backend/models/user.model.js
@@ -0,0 +1,30 @@
+import mongoose, { Schema } from "mongoose";
+
+const userSchema = new Schema(
+    {
+        name : {
+            type: String,
+            required: true,
+            trim: true
+        },
+        username : {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        email : {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+        refreshToken : {
+            type : String
+        }
+    },{timestamps: true}
+)
+
+export const User = mongoose.model("User",userSchema) 
